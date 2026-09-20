254d8a96ad05426b7bbb25f7227b375d71822367
Laila-flutter <lailamohammadabdulaziz@gmail.com>
2026-09-20 21:03:16 +0300

    Updated website design

--- js/script.js
+++ js/script.js
@@ -8,3 +8,28 @@
 //         // Optional: Reset form fields after successful submission
 //         this.reset();
 //     });
+
+
+
+
+
+
+const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+const appendAlert = (message, type) => {
+  const wrapper = document.createElement('div')
+  wrapper.innerHTML = [
+    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+    `   <div>${message}</div>`,
+    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+    '</div>'
+  ].join('')
+
+  alertPlaceholder.append(wrapper)
+}
+
+const alertTrigger = document.getElementById('liveAlertBtn')
+if (alertTrigger) {
+  alertTrigger.addEventListener('click', () => {
+    appendAlert('Thank you for your message! We will get back to you soon.', 'success')
+  })
+}
